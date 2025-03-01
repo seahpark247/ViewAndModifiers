5c096c8b7059a1dd671eaa3314848f6f7c993aba
seahpark247 <famousjedo@gmail.com>
2025-03-01 11:21:23 -0600

    Initial Commit

---
+++ ViewAndModifiers/ViewAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewAndModifiersApp.swift
+//  ViewAndModifiers
+//
+//  Created by Seah Park on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ViewAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ViewAndModifiersTests/ViewAndModifiersTests.swift
@@ -0,0 +1,17 @@
+//
+//  ViewAndModifiersTests.swift
+//  ViewAndModifiersTests
+//
+//  Created by Seah Park on 3/1/25.
+//
+
+import Testing
+@testable import ViewAndModifiers
+
+struct ViewAndModifiersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
